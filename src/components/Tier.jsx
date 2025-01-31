a505a0b756db1f9fe0c20a44af1781327689ee31
fauzan.lisan <fauzan.lisan@solusi247.com>
2025-01-31 13:40:33 +0700

    setting color in icon when dark

--- src/components/Tier.jsx
+++ src/components/Tier.jsx
@@ -15,7 +15,7 @@ const Tier = ({ tier, color, skills, theme }) => {
             <img
               key={i}
               width={'80px'}
-              className={`aspect-[3/2] object-contain drop-shadow-xl rounded-full p-1 ${theme == "dark" ? "bg-gray-300 shadow-[0_0_2px_1px] shadow-gray-200" : ""}`}
+              className={`aspect-[3/2] object-contain drop-shadow-xl rounded-full p-1 ${theme == "dark" ? "bg-gray-400 shadow-[0_0_2px_1px] shadow-gray-300" : ""}`}
               loading="lazy"
               src={getImageSkill(skill.image)}
               alt={skill.name}
